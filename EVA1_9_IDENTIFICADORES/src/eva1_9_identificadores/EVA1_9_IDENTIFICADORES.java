1a877432b2495c21e4e97e34fa23809aa35f701b
non209 <143562722+non209@users.noreply.github.com>
2023-09-13 17:53:34 -0600

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/src/eva1_9_identificadores/EVA1_9_IDENTIFICADORES.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_9_identificadores;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_9_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int $nombre;
+        int _nombre;
+        int nombre1; 
+        int nombre;
+        int Nombre;
+        int nombre_dato;
+        //int nombre; la variable ya existe previamente
+        
+        //int public; no podemo usar public, esla palabra reservada (keyword)
+        int Public;
+        
+        System.out.println("hola");
+        System.out.println("como estas?");
+    }
+
+}
