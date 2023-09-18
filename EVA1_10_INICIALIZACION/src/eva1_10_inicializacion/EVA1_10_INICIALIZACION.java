43ca99d4a067b0bf0f11bf18cd198391b7465328
non209 <143562722+non209@users.noreply.github.com>
2023-09-18 17:53:19 -0600

    Add files via upload

---
+++ EVA1_10_INICIALIZACION/src/eva1_10_inicializacion/EVA1_10_INICIALIZACION.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_10_inicializacion;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_10_INICIALIZACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor = 0;
+        System.out.println(valor);
+
+        int precio;
+        precio = 100;
+
+        double salario = 50, preciomayoreo = 100, iva = 16, subtotal = 15, total = 0;
+
+        //CONSTANTES 
+        //final--> TE PERMITE ASIGANR UN VALOR SOLO UNA VEZ 
+        final int conteo;
+        conteo = 100;
+        System.out.println(conteo);
+
+        final double MI_VALOR_PI = 3.1416;
+        final String ESCUELA = "Instituto Tecnologico de Chihuahua II";
+        final int CALIFA_PASAR = 70;
+
+        /*gg*/
+        int x; //gg
+    }
+
+}
